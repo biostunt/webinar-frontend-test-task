dab4f228c95c65582fc26735e0718a7a4b6ac09b
biostunt <nice.nikita99@mail.ru>
2021-07-30 17:27:29 +0300

    attach immer.js to project. add immer to reducer

--- src/contexts/TodoItems/reducer.ts
+++ src/contexts/TodoItems/reducer.ts
@@ -1,5 +1,6 @@
 import { TodoItemsAction } from "./actions";
 import { TodoItemsState } from "./state";
+import produce from "immer";
 
 function generateId() {
     return `${Date.now().toString(36)}-${Math.floor(
@@ -7,44 +8,45 @@ function generateId() {
     ).toString(36)}`;
 }
 
-function reducer(
+const reducer = (
     state: TodoItemsState,
     action: TodoItemsAction
-): TodoItemsState {
-    switch (action.type) {
-        case "loadState":
-            return action.data;
-        case "add":
-            return {
-                ...state,
-                todoItems: [
-                    { id: generateId(), done: false, ...action.data },
-                    ...state.todoItems,
-                ],
-            };
-        case "delete":
-            return {
-                ...state,
-                todoItems: state.todoItems.filter(
-                    ({ id }) => id !== action.data
-                ),
-            };
-        case "toggleDone":
-            const itemIndex = state.todoItems.findIndex(
-                ({ id }) => id === action.data
-            );
-            const item = state.todoItems[itemIndex];
-            return {
-                ...state,
-                todoItems: [
-                    ...state.todoItems.slice(0, itemIndex),
-                    { ...item, done: !item.done },
-                    ...state.todoItems.slice(itemIndex + 1),
-                ],
-            };
-        default:
-            return state;
-    }
-}
+): TodoItemsState =>
+    produce(state, (draftState) => {
+        switch (action.type) {
+            case "loadState":
+                return action.data;
+            case "add":
+                return {
+                    ...draftState,
+                    todoItems: [
+                        { id: generateId(), done: false, ...action.data },
+                        ...draftState.todoItems,
+                    ],
+                };
+            case "delete":
+                return {
+                    ...draftState,
+                    todoItems: draftState.todoItems.filter(
+                        ({ id }) => id !== action.data
+                    ),
+                };
+            case "toggleDone":
+                const itemIndex = draftState.todoItems.findIndex(
+                    ({ id }) => id === action.data
+                );
+                const item = draftState.todoItems[itemIndex];
+                return {
+                    ...draftState,
+                    todoItems: [
+                        ...draftState.todoItems.slice(0, itemIndex),
+                        { ...item, done: !item.done },
+                        ...draftState.todoItems.slice(itemIndex + 1),
+                    ],
+                };
+            default:
+                return state;
+        }
+    });
 
 export default reducer;
